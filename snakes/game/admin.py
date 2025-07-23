db6e662c37c8fb748cee4056ffb7e666cabaa03d
Brian Aguinsaca <brian.aguinsaca@unl.edu.ec>
2025-07-23 14:55:05 -0500

    Clase admin

--- snakes/game/admin.py
+++ snakes/game/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Score
 
+#Esta clase se encarga de administrar el modelo Score en el panel de administración de Django
 @admin.register(Score)
 class ScoreAdmin(admin.ModelAdmin):
     list_display = ('player_name', 'score', 'created_at')
